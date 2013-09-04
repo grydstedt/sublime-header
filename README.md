d2ae29393109879c6506308b6015d643cad2d11f
Gustav <gustav.rydstedt@gmail.com>
2013-09-04 10:12:44 -0700

    Change to block style comment

---
+++ README.md
@@ -0,0 +1,4 @@
+Header file insert plugin for Sublime 2
+=====================================
+
+Creates header snippets for your
--- insert_header.py
+++ insert_header.py
@@ -8,11 +8,12 @@ class InsertHeaderCommand(sublime_plugin.TextCommand):
     project = self.view.settings().get('project_name', '')
     company = self.view.settings().get('company_name')
 
-    header = ('// %s\n'
-            '// %s\n'
-            '//\n'
-            '// Created by %s on %s.\n'
-            '// Copyright (c) %s %s. All rights reserved.\n'
+    header = ('/* %s\n'
+            '* %s\n'
+            '*\n'
+            '* Created by %s on %s.\n'
+            '* Copyright (c) %s %s. All rights reserved.\n'
+            '*/\n'
             '\n'
             ) % (
             os.path.basename(self.view.file_name() or 'Untitled'), 
